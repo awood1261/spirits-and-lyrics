6770d72ab0181640b4784245731efdee05e3d3bc
Alex Wood <alex.c.wood@gmail.com>
2019-05-25 23:17:33 -0400

    adds first component for header, yay!

---
+++ src/components/SnlHeader/SnlHeader.js
@@ -0,0 +1,11 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const SnlHeader = () => (
+    <header>
+        <h1>This is the title for Spirits &amp; Lyrics.</h1>
+    </header>
+)
+
+export default SnlHeader
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import SnlHeader from "./SnlHeader/SnlHeader"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -25,6 +26,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <SnlHeader />
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
